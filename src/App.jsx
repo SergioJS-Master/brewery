5ee5163cdee2074af97d60fdeb1423ca5ef4e7cb
SergioJS-Master <Sergio.Krapchini@yandex.ru>
2023-03-10 14:15:28 +0300

    Изменения в App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header/Header'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="container">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
