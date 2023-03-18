74c8707a1389b2366f69be13780f09b90ca64649
Mikhail <bloodsmiley130391@gmail.com>
2023-03-18 15:18:30 +0300

    добавляет модалку 18+

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
+import { ModalWarning } from './components/ModalWarning/ModalWarning'
 
 function App() {
   return (
     <div className="container">
+      <ModalWarning />
       <Header />
       <Outlet />
       <Footer />
