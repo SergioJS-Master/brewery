191da06122c39d445ba0208e8baefeddc9f2306b
SergioJS-Master <Sergio.Krapchini@yandex.ru>
2023-03-21 21:31:56 +0300

    доработка раздела News

--- src/API/newsApi.js
+++ src/API/newsApi.js
@@ -2,14 +2,47 @@
 export const listPicturesNews = [
   {
     img: 'https://a.d-cd.net/180922es-1920.jpg',
+    name: 'OCTOBERFEST',
     discription: 'Light lager with moderate bitterness according to traditional recipes of American light lager',
   },
   {
     img: 'https://avatars.dzeninfra.ru/get-zen_doc/3137181/pub_60fe5f57563df2218123acef_60fe68153b939a51fa43972d/scale_1200',
+    name: 'PARTY AND BULSHIT',
     discription: 'Light lager with moderate bitterness according to traditional',
   },
   {
     img: 'https://proza.ru/pics/2020/03/23/1602.jpg',
+    name: 'BEER FEST',
+    discription: 'Light lager with moderate according to traditional recipes of American light lager',
+  },
+  {
+    img: 'https://a.d-cd.net/180922es-1920.jpg',
+    name: 'OCTOBERFEST',
+    discription: 'Light lager with moderate bitterness according to traditional recipes of American light lager',
+  },
+  {
+    img: 'https://avatars.dzeninfra.ru/get-zen_doc/3137181/pub_60fe5f57563df2218123acef_60fe68153b939a51fa43972d/scale_1200',
+    name: 'PARTYANDBULSHIT',
+    discription: 'Light lager with moderate bitterness according to traditional',
+  },
+  {
+    img: 'https://proza.ru/pics/2020/03/23/1602.jpg',
+    name: 'BEER FEST',
+    discription: 'Light lager with moderate according to traditional recipes of American light lager',
+  },
+  {
+    img: 'https://a.d-cd.net/180922es-1920.jpg',
+    name: 'OCTOBERFEST',
+    discription: 'Light lager with moderate bitterness according to traditional recipes of American light lager',
+  },
+  {
+    img: 'https://avatars.dzeninfra.ru/get-zen_doc/3137181/pub_60fe5f57563df2218123acef_60fe68153b939a51fa43972d/scale_1200',
+    name: 'PARTYANDBULSHIT',
+    discription: 'Light lager with moderate bitterness according to traditional',
+  },
+  {
+    img: 'https://proza.ru/pics/2020/03/23/1602.jpg',
+    name: 'BEER FEST',
     discription: 'Light lager with moderate according to traditional recipes of American light lager',
   },
 ]
--- src/components/Pages/News/News.jsx
+++ src/components/Pages/News/News.jsx
@@ -1,34 +1,66 @@
+/* eslint-disable consistent-return */
 /* eslint-disable max-len */
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 // import { View } from 'react-native'
 // import { Card, Title } from 'react-native-paper'
+import { useEffect, useState } from 'react'
 import styles from './News.module.css'
 
 import { listPicturesNews } from '../../../API/newsApi'
 
 export function News() {
-  const [randomImage, setRandomImage] = useState('')
+  const [imageIndex, setImageIndex] = useState(0)
 
-  console.log(randomImage)
-
-  const renderImage = () => {
-    const randomImageIndex = Math.floor(Math.random() * Math.floor(3))
-    return listPicturesNews[randomImageIndex].img
-  }
+  const des = listPicturesNews.map((el) => el.img)
+  const des2 = listPicturesNews.map((el) => el.name)
+  const des3 = listPicturesNews.map((el) => el.discription)
 
   useEffect(() => {
-    setRandomImage(renderImage)
-  })
+    setInterval(() => {
+      setImageIndex((prev) => (
+        prev === des.length - 1 ? 0 : prev + 1
+      ))
+    }, 6000)
+  }, [])
 
   return (
-    <div style={styles.container}>
-      <div>
-        <div>
-          <p>React Native Random Image</p>
-        </div>
-        <img src={renderImage()} alt="" />
-      </div>
-    </div>
+    <>
 
+      <div className={styles.newsContainer}>
+        <div className={styles.changeImgContantainer}>
+          <div className={styles.changeImgContant}>
+            <div>
+              <h1 className={styles.newsHeader}>NEWS</h1>
+              <hr />
+            </div>
+            <img
+              className={styles.changeImg}
+              src={des[imageIndex]}
+              alt="logo"
+            />
+            <h1 className={styles.changeImgHeader}>{des2[imageIndex]}</h1>
+            <h1 className={styles.changeImgDiscription}>{des3[imageIndex]}</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.newsCardsContantainer}>
+        <hr className={styles.hrNewsCards} />
+        <div className={styles.newsCards}>
+          {listPicturesNews.map((el) => (
+            <div className={styles.newsCardsContent}>
+              <h1 className={styles.newsCardsH1}>{el.name}</h1>
+              <img
+                className={styles.newsCardsImg}
+                src={el.img}
+                alt=""
+              />
+              <p className={styles.newsCardsDiscription}>{el.discription}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </>
   )
 }
