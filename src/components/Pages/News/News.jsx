51577ab5770a732ce61c5ea6eefe49bb0898cb3d
SergioJS-Master <Sergio.Krapchini@yandex.ru>
2023-03-22 18:44:32 +0300

    Добавил анимацию и доработал стили в News

--- src/components/Pages/News/News.jsx
+++ src/components/Pages/News/News.jsx
@@ -4,7 +4,9 @@
 // import { View } from 'react-native'
 // import { Card, Title } from 'react-native-paper'
 import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import styles from './News.module.css'
+
 import { listPicturesNews } from '../../../API/newsApi'
 
 export function News() {
@@ -16,7 +18,9 @@ export function News() {
 
   useEffect(() => {
     setInterval(() => {
-      setImageIndex((prev) => (prev === des.length - 1 ? 0 : prev + 1))
+      setImageIndex((prev) => (
+        prev === des.length - 1 ? 0 : prev + 1
+      ))
     }, 6000)
   }, [])
 
@@ -24,14 +28,43 @@ export function News() {
     <>
       <div className={styles.newsContainer}>
         <div className={styles.changeImgContantainer}>
-          <div className={styles.changeImgContant}>
+          <div
+            className={styles.changeImgContant}
+          >
             <div>
-              <h1 className={styles.newsHeader}>NEWS</h1>
+              <h1
+                className={styles.newsHeader}
+              >
+                NEWS
+              </h1>
               <hr />
             </div>
-            <img className={styles.changeImg} src={des[imageIndex]} alt="logo" />
-            <h1 className={styles.changeImgHeader}>{des2[imageIndex]}</h1>
-            <h1 className={styles.changeImgDiscription}>{des3[imageIndex]}</h1>
+            <motion.img
+              className={styles.changeImg}
+              src={des[imageIndex]}
+              alt="logo"
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 1.5, repeatDelay: 4.5, repeat: Infinity }}
+
+            />
+            <motion.h1
+              initial={{ opacity: 0, x: 1000 }}
+              transition={{ duration: 2 }}
+              animate={{ opacity: 1, x: 0 }}
+              className={styles.changeImgHeader}
+            >
+              {des2[imageIndex]}
+
+            </motion.h1>
+            <motion.h1
+              initial={{ opacity: 0, x: -1000 }}
+              transition={{ duration: 2 }}
+              animate={{ opacity: 1, x: 0 }}
+              className={styles.changeImgDiscription}
+            >
+              {des3[imageIndex]}
+
+            </motion.h1>
           </div>
         </div>
       </div>
@@ -42,12 +75,17 @@ export function News() {
           {listPicturesNews.map((el) => (
             <div className={styles.newsCardsContent}>
               <h1 className={styles.newsCardsH1}>{el.name}</h1>
-              <img className={styles.newsCardsImg} src={el.img} alt="" />
+              <img
+                className={styles.newsCardsImg}
+                src={el.img}
+                alt=""
+              />
               <p className={styles.newsCardsDiscription}>{el.discription}</p>
             </div>
           ))}
         </div>
       </div>
+
     </>
   )
 }
